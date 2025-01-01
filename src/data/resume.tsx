028e257059672d9b369d613c120cf9bf15a04495
AaraV <aaravrawat00000@gmail.com>
2025-01-01 12:42:51 +0530

    projects added and their links and meta data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,6 +2,7 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
+
   name: "Aarav",
   initials: "AR",
   url: "https://linktr.ee/AaravRawat",
@@ -11,36 +12,38 @@ export const DATA = {
     "On a journey to becoming a Product Builder.Open to Internship/Freelance",
   summary:
     "I'm a [Computer science & Bussines student](/#education), Who is passionate about [Web3 & Starup-Domain](/#). In this journey i build [Freelance projects & Products](/#projects). Currently i'm [Mastering Web Dev • Learning DevOps • Passionate for Web3](/#skills)",
+
   avatarUrl: "/me.jpg",
+
   skills: [
-   "DSA",
-  "OOPs",
-  "STL",
-  "C",
-  "C++",
-  "JavaScript",
-  "TypeScript",
-  "ReactJS",
-  "NextJS",
-  "ExpressJS",
-  "NodeJS",
-  "HonoJS",
-  "TailwindCSS",
-  "Aceternity UI",
-  "Shadcn UI",
-  "MongoDB",
-  "PostgreSQL",
-  "NeonDB",
-  "AivenDB",
-  "AWS EC2 S3",
-  "Cloudflare Workers",
-  "Docker",
-  "Git/GitHub",
-  "Postman",
-  "Prisma",
-  "Zod",
-  "TurboRepo",
-  "NextAuth"
+    "DSA",
+    "OOPs",
+    "STL",
+    "C",
+    "C++",
+    "JavaScript",
+    "TypeScript",
+    "ReactJS",
+    "NextJS",
+    "ExpressJS",
+    "NodeJS",
+    "HonoJS",
+    "TailwindCSS",
+    "Aceternity UI",
+    "Shadcn UI",
+    "MongoDB",
+    "PostgreSQL",
+    "NeonDB",
+    "AivenDB",
+    "AWS EC2 S3",
+    "Cloudflare Workers",
+    "Docker",
+    "Git/GitHub",
+    "Postman",
+    "Prisma",
+    "Zod",
+    "TurboRepo",
+    "NextAuth"
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
@@ -78,7 +81,7 @@ export const DATA = {
 
         navbar: true,
       },
-      socialHandle:{
+      socialHandle: {
         url: "https://linktr.ee/AaravRawat"
       }
     },
@@ -117,131 +120,135 @@ export const DATA = {
     }
   ],
   projects: [
+
     {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
+      title: "Paytm",
+      href: "https://github.com/Aarav-Rawat/Paytm",
+      dates: "Dec 2024 - Present",
       active: true,
       description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+        "",
       technologies: [
         "Next.js",
         "Typescript",
+        "Docker",
+        'Express.js',
         "PostgreSQL",
         "Prisma",
         "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
       ],
       links: [
-        {
-          type: "Website",
-          href: "https://chatcollect.com",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
-    {
-      title: "Magic UI",
-      href: "https://magicui.design",
-      dates: "June 2023 - Present",
-      active: true,
-      description:
-        "Designed, developed and sold animated UI components for developers.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-        
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://magicui.design",
-          icon: <Icons.globe className="size-3" />,
-        },
         {
           type: "Source",
-          href: "https://github.com/magicuidesign/magicui",
+          href: "https://github.com/Aarav-Rawat/Paytm",
           icon: <Icons.github className="size-3" />,
         },
       ],
       image: "",
-      video: "https://cdn.magicui.design/bento-grid.mp4",
+      video:
+        "",
     },
+
     {
-      title: "llm.report",
-      href: "https://llm.report",
-      dates: "April 2023 - September 2023",
+      title: "DailyCombos - Freelance",
+      href: "https://medium-aarav.vercel.app/",
+      dates: "",
       active: true,
       description:
-        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+        "",
       technologies: [
-        "Next.js",
+        "React.js",
+        "Hono.js",
+        "CloudFlare Workers",
         "Typescript",
         "PostgreSQL",
         "Prisma",
         "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
       ],
       links: [
         {
           type: "Website",
-          href: "https://llm.report",
+          href: "https://medium-aarav.vercel.app/",
           icon: <Icons.globe className="size-3" />,
         },
         {
           type: "Source",
-          href: "https://github.com/dillionverma/llm.report",
+          href: "https://github.com/Aarav-Rawat/Medium",
           icon: <Icons.github className="size-3" />,
         },
       ],
       image: "",
-      video: "https://cdn.llm.report/openai-demo.mp4",
+      video: "",
     },
+
     {
-      title: "Automatic Chat",
-      href: "https://automatic.chat",
-      dates: "April 2023 - March 2024",
+      title: "Medium",
+      href: "https://medium-aarav.vercel.app/",
+      dates: "Nov 2024 - Dec 2024",
       active: true,
       description:
-        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+        "",
       technologies: [
-        "Next.js",
+        "React.js",
+        "Hono.js",
+        "CloudFlare Workers",
         "Typescript",
         "PostgreSQL",
         "Prisma",
         "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
       ],
       links: [
         {
           type: "Website",
-          href: "https://automatic.chat",
+          href: "https://medium-aarav.vercel.app/",
           icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/Aarav-Rawat/Medium",
+          icon: <Icons.github className="size-3" />,
         },
       ],
       image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+      video: "",
     },
+
+    {
+      title: "Client Portfolio - Freelance",
+      href: "https://medium-aarav.vercel.app/",
+      dates: "",
+      active: true,
+      description:
+        "",
+      technologies: [
+        "React.js",
+        "Hono.js",
+        "CloudFlare Workers",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://medium-aarav.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/Aarav-Rawat/Medium",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "",
+    },
+    
   ],
+
   hackathons: [
-   
+
   ],
+
 } as const;
