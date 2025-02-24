a5640ed6088abe35766fa1d0e210aea5a86b87be
Aarav-Rawat <aaravrawat00000@gmail.com>
2025-02-24 18:00:48 +0530

    kuch tho kara h

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -10,9 +10,9 @@ export const DATA = {
   location: "Dill, India",
   locationLink: "https://www.google.com/maps/place/NewDelhi",
   description:
-    "On a journey to becoming a Product Builder. Open to Job/Freelance",
+    "I turn Vision into Product. Open to Job/Freelance",
   summary:
-    "I'm a [Developer](/#projects), Who is passionate about [Tech](/#skills) and have hands-on experience building full-stack applications using [Next.js, React, DevOps, Express](/#skills), and databases like [PostgreSQL & MongoDB](/#skills). Additionally, I have worked with [Docker, CI/CD, APIs, UI libraries, and AI tools](/#skills).Throughout this journey i build [Client-Projects](/#projects) gaining hands-on [Experience](/#skills) with diverse [Technologies](/#skills) Also have a strong foundation in DSA, OOPs, and problem-solving.I am continiously adapting to new [Environment](/#skills).",
+    "I'm a [Software Engineer](/#projects), Who is passionate about [Tech](/#skills) and have hands-on experience building full-stack applications using [Next.js](/#skills) Additionally, I have worked with [DevOps and AI tools](/#skills).Throughout this journey i build [Client-Projects](/#projects) gaining hands-on [Experience](/#skills) with diverse [Technologies](/#skills) Also have a strong foundation in DSA, OOPs, and problem-solving and continiously adapting to new [Environment](/#skills).",
 
   avatarUrl: "/me.jpg",
 
